491008b066b7faa1b692ccac7b3bf7c4457655ce
ijlee2 <ijlee2@users.noreply.github.com>
2018-06-02 15:25:17 -0500

    Added clear button and styled it

--- components/HeaderBar.js
+++ components/HeaderBar.js
@@ -1,34 +1,68 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Dimensions, TouchableHighlight } from 'react-native';
+
+const { height: screenHeight, width: screenWidth } = Dimensions.get('window');
 
 export default class HeaderBar extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>{this.props.displayText}</Text>
+      <View style={styles.headerContainer}>
+        <View style={styles.displayTextContainer}>
+          <Text style={styles.displayText}>{this.props.displayText}</Text>
+        </View>
+
+        <View style={styles.clearButtonContainer}>
+          <TouchableHighlight style={styles.clearButtonTouchable} onPress={this.props.clearDisplayText}>
+            <Text style={styles.clearButtonText}>X</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#f5f5f5',
+  headerContainer: {
+    backgroundColor: 'green',
 
     ...Platform.select({
       ios: {
-        top: 100,
-        height: '20%'
+        backgroundColor: '#f5f5f5',
+        color: '#000',
+        top: 50,
+        height: screenHeight * 0.05
       },
       android: {
+        backgroundColor: 'blue',
         height: '200'
       },
     }),
+
+    display: 'flex',
+    flexDirection: 'row'
   },
 
-  text: {
-    color: 'blue',
+
+  displayTextContainer: {
+    flexGrow: 9,
+    maxWidth: 0.8 * screenWidth
+  },
+  displayText: {
+    color: '#000',
     fontWeight: 'bold',
-    fontSize: 30,
+    fontSize: 24,
+  },
+
+  clearButtonContainer: {
+    flexGrow: 1
+  },
+  clearButtonTouchable: {
+    alignItems: 'center',
+    backgroundColor: '#232323',
+    padding: 10,
+  },
+  clearButtonText: {
+    color: "#eee",
+    fontSize: 18,
   }
 });
