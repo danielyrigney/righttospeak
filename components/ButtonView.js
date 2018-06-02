65d1cc1c39510958873fb3967f7dc0724d023a0c
Daniel Rigney <danielrigney@Daniels-MacBook-Air.local>
2018-06-02 18:48:30 -0500

    re-uploaded assests

--- components/ButtonView.js
+++ components/ButtonView.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
 import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+const images = {
+  I: require('../assets/I/I1_P.bmp'),
+  me: require('../assets/M/me5.bmp'),
+  my: require('../assets/M/my2_P.bmp'),
+  want: require('../assets/W/want_P.bmp'),
+  him: require('../assets/H/him2_P.bmp'),
+  her: require('../assets/H/her3_P.bmp'),
+  she: require('../assets/S/she1_P.bmp'),
+  he: require('../assets/H/he1_P.bmp'),
+  like: require('../assets/L/like_P.bmp'),
+  stop: require('../assets/S/stop_P.bmp'),
+  come: require('../assets/C/come_P.bmp'),
+  go: require('../assets/G/go_P.bmp'),
+  away: require('../assets/A/away2_P.bmp'),
+  any: require('../assets/A/any_P.bmp'),
+  nothing: require('../assets/N/nothing_P.bmp'),
+  hello: require('../assets/H/hello_P.bmp'),
+  goodbye: require('../assets/G/goodbye_P.bmp'),
+  little: require('../assets/L/little_P.bmp'),
+  big: require('../assets/B/big_P.bmp'),
+  wait: require('../assets/W/wait_P.bmp'),
+  over: require('../assets/O/over_P.bmp'),
+  under: require('../assets/U/under_P.bmp'),
+  left: require('../assets/L/left1_P.bmp'),
+  right: require('../assets/R/right1a_P.bmp'),
+  leave: require('../assets/L/leave_P.bmp'),
+  all: require('../assets/A/all_P.bmp'),
+  food: require('../assets/F/food_P.bmp'),
+  play: require('../assets/P/play_P.bmp'),
+  feel: require('../assets/F/feel_P.bmp'),
+  thing: require('../assets/T/thing_P.bmp'),
+};
+
+
+
+
 export default class ButtonView extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             button: {
-                text: 'Hello!'
+                text: 'like',
+                image: 'like',
             }
         };
     }
+
 
     updateDisplayText = () => {
         const { text } = this.state.button;
@@ -23,8 +61,8 @@ export default class ButtonView extends Component {
             <View style={styles.container}>
                 <TouchableOpacity title="" onPress={this.updateDisplayText} >
                     <View>
-                        <ButtonTitle />
-                        <ButtonImage />
+                        <ButtonTitle title={this.state.button.text}/>
+                        <ButtonImage path={this.state.button.image}/>
                     </View>
                 </TouchableOpacity>
             </View>
@@ -32,23 +70,18 @@ export default class ButtonView extends Component {
     }
 }
 
-const ButtonTitle = () => {
-    const titleText = "changing the title";
-
+const ButtonTitle = (props) => {
+    const titleText = props.title;
     return (
         <Text style={styles.text}>{`${titleText}`}</Text>
     )
 };
 
-const ButtonImage = () => {
-    //const icon = this.props;
-    const iconFilePath = "A/a_P.bmp";
-
+const ButtonImage = (props) => {
     return (
-        <Image source={require(`../assets/${iconFilePath}`)} resizeMode="contain" />
+      <Image source={images[props.path]} resizeMode="contain" />
     )
 };
-
 
 const styles = StyleSheet.create({
     container: {
@@ -57,6 +90,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flexDirection: 'row',
         justifyContent: 'center',
+        borderRadius: 20,
+        borderWidth: 10,
+        borderColor: 'black',
     },
     text: {
         textAlign: 'center',
