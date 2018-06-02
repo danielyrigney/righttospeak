0e9003d331acd346a57951e6cb28909504c2e5d6
Victor Rojas <victor@justlegal.com>
2018-06-02 17:13:44 -0500

    return ButtonTitle ButtonImage

--- components/ButtonView.js
+++ components/ButtonView.js
@@ -23,6 +23,8 @@ export default class ButtonView extends Component {
             <View style={styles.container}>
                 <TouchableOpacity title="" onPress={this.updateDisplayText} >
                     <View>
+                        <ButtonTitle />
+                        <ButtonImage />
                         <Text style={styles.text}> {this.props.text} </Text>
                     </View>
                 </TouchableOpacity>
